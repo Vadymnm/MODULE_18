890f25e502b6c743265d1c45bd14001d336674eb
Vadym <telcom.odessa@gmail.com>
2024-09-10 17:00:54 +0300

    Initial

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,18 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-#from task2.views import index,index2
-from task2.views import index
+from task3.views import index, games
 from django.views.generic import TemplateView
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', index),
-#     path('index/', index2.as_view()),
-# ]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    path('index/', TemplateView.as_view(template_name='class_template.html')),
+    path('games/',games),
+
+    path('cart/', TemplateView.as_view(template_name='cart.html')),
 ]
